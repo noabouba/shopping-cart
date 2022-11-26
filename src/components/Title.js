bb9db45970183bae782406a368c5605a9aaa2bb6
Hadas <hadas716@gmail.com>
2022-11-26 15:31:39 +0200

    props fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Title({name , title}) {
+export default function Title(props) {
     return (
         <div className="row">
             <div className="col-10 mx-auto text-center text-title">
                 <h1 className="text-capitalize font-weight-bold">
-                    {name}  <strong className="text-blue">{title}</strong>
+                    {props.name}  <strong className="text-blue">{props.title}</strong>
                 </h1>
             </div>
         </div>
