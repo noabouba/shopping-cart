58c3e65c772045461173b18c320d9d2b1196d562
Hadas <hadas716@gmail.com>
2022-11-26 13:54:23 +0200

    switch to functions

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
-import styled from 'styled-components';
+import React from 'react';
 import {Link} from 'react-router-dom';
+import './Product.css';
 
-export default class Product extends Component {
-    render() {
-        const {id, title, firstImage, price} = this.props.product;
-        
-        return (
-            <ProducrWrapper className="col-9 mx-auto col-md-5 my-2">
-                <Link to={`details/${id}`} style={{"textDecoration": "none", "color":"inherit"}}>
+function Product(props) {
+    const {id, title, firstImage, price} = props.product;
+    
+    return (
+        <div className="col-9 mx-auto col-md-5 my-2">
+            <Link to={`details/${id}`} style={{"textDecoration": "none", "color":"inherit"}}>
                 <div className="card">
                     <div className="card-footer d-flex justify-content-between">
                         <p className="align-self-center mb-0">
@@ -24,41 +23,9 @@ export default class Product extends Component {
                         </h5>
                     </div>
                 </div>
-                </Link>
-            </ProducrWrapper>
-        );
-    }
+            </Link>
+        </div>
+    );
 }
 
-const ProducrWrapper =styled.div`
-img{
-    height:100px;
-    width:100px;
-}
-.card{
-    border-color:tranparent;
-    transition:all 1s linear;
-    background-color:#f6f6f6;
-}
-.card-footer{
-    background:transparent;
-    border-top:transparent;
-    transition:all 1s linear;
-    padding:inherit !important;
-    padding-left:5% !important;
-}
-&:hover{
-    .card{
-        border:0.04rem solid rgba(0,0,0,0.2);
-        cursor:pointer;
-        box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
-    }
-    .card-footer{
-        background:rgba(247,247,247);
-    }
-}
-.img-container:hover .cart-btn{
-    transform:translate(0, 0);
-}
-
-`;
+export default Product;
