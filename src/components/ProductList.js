5ed0945dc2eccf3ca30beeec3a2b2cf08d6d4d74
Hadas <hadas716@gmail.com>
2022-12-05 20:08:34 +0200

    get products from server- need work

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react';
 import Product from "./Product";
 import Title from "./Title";
 import {storeProducts} from '../data.js';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 function ProductList() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/products", {
+            method: 'GET',
+            headers: {
+            accept: 'application/json',
+            },
+        })
+        .then((res) => console.log("dgbhjhj"))
+        // .then((data) => {console.log("dd"); setProducts(data.message); })
+        .catch(error => {
+            console.error('There was an error!', error);
+        });;
+    }, []);
+
     return (
         <div className="py-3">
             <div className="container">
                 <Title name="our" title="Shoes"/>
                 <div className="row">
                     { storeProducts.map(product =>{
-                        return <Product product={product} />;
+                        return <Product product={product} key={product.id} isCartView={false}/>;
                     })
                     }
                 </div>
