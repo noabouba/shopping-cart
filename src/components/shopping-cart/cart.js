ff91ab979cf899021891c014b8855cb58976df8e
Hadas <hadas716@gmail.com>
2022-12-08 11:34:01 +0200

    fix ux and add title

--- src/components/shopping-cart/cart.js
+++ src/components/shopping-cart/cart.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ProductList from '../ProductList.js';
+import Title from "../Title";
 
 function Cart() {
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
 
     return (
         <div>
+            <Title name="Your" title="Order"/>
             <ProductList products={cart} isInCart={true}></ProductList>
         </div>
     );
