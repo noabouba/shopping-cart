d116d430663f69e2e7841e7d818ed7dcccde3d31
noabouba <boubanoa@gmail.com>
2022-12-09 11:11:21 +0200

    wip - css for shopping cart

--- src/components/shopping-cart/cart.js
+++ src/components/shopping-cart/cart.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import Product from "../Product";
+import Product from '../product/Product';
+import './cart.css'
 
 function Cart(props) {
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
 
     return (
         <div className="py-3">
+            <h2 class="cart-title">Shopping Bag</h2>
             <div className="container">
                 <div className="row">
                     { cart?.map(product =>{
